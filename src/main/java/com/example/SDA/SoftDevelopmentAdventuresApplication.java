5cf456ce7124585646399429d7312497f9a22eda
Paweł <pawelflis84@gmail.com>
2023-04-27 19:48:25 +0200

    git init

---
+++ src/main/java/com/example/SDA/SoftDevelopmentAdventuresApplication.java
@@ -0,0 +1,13 @@
+package com.example.SDA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftDevelopmentAdventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftDevelopmentAdventuresApplication.class, args);
+	}
+
+}
